b9b8d22903d43d150ca4a64326b2da2791923ff6
Charles <charleswonghm@gmail.com>
2021-03-23 18:37:30 -0400

    Added GUI

--- Assets/Scripts/GeneratePoints.cs
+++ Assets/Scripts/GeneratePoints.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class GeneratePoints : MonoBehaviour
 {
-    [Range(1, 300)]
-    public int n;
+    [SerializeField][Range(1, 300)]
+    private int n;
 
-    [Range(0f, 3f)]
-    public float turnFraction = (1 + Mathf.Sqrt(5)) / 2;
+    [SerializeField][Range(0f, 3f)]
+    private float turnFraction;
 
-    public int radius;
+    private int radius;
 
     public GameObject point;
 
-    private int n_cache;
-    private float turn_cache;
+    // Used during development when adjusting values via Inspector
+    // private int n_cache;
+    // private float turn_cache;
+
     private GameObject[] points;
 
     // Start is called before the first frame update
     void Start()
     {
         points = new GameObject[300];
+        turnFraction = (1 + Mathf.Sqrt(5)) / 2;
+        n = 300;
+        radius = 30;
+
         for (int i = 0; i < 300; i++) {
             points[i] = Object.Instantiate(point, Vector3.zero, Quaternion.identity);
         }
@@ -36,17 +42,41 @@ public class GeneratePoints : MonoBehaviour
             points[i].transform.position = new Vector3(x, y, 0);
         }
 
-        n_cache = n;
-        turn_cache = turnFraction;
+        // n_cache = n;
+        // turn_cache = turnFraction;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (n != n_cache || turnFraction != turn_cache) {
+    // Used during development when adjusting values via Inspector
+    // void Update()
+    // {
+    //     if (n != n_cache || turnFraction != turn_cache) {
 
-            for (int i = 0; i < n; i++) {
-            float dst = Mathf.Sqrt((i + 0.5f) / n) * radius;
+    //         for (int i = 0; i < n; i++) {
+    //         float dst = Mathf.Sqrt((i + 0.5f) / n) * radius;
+    //         float theta = 2 * Mathf.PI * i * turnFraction; // (1 + Mathf.Sqrt(5));
+
+    //         float x = dst * Mathf.Cos(theta);
+    //         float y = dst * Mathf.Sin(theta);
+
+    //         points[i].SetActive(true);
+    //         points[i].transform.position = new Vector3(x, y, 0);
+    //         }
+
+    //         for (int i = n; i < 300; i++) {
+    //             points[i].SetActive(false);
+    //         }
+
+    //         n_cache = n;
+    //         turn_cache = turnFraction;
+    //     }
+    // }
+
+    public void RefreshNumPoints(float numPoints)
+    {
+        if (numPoints < 300) {
+
+            for (int i = 0; i < numPoints; i++) {
+            float dst = Mathf.Sqrt((i + 0.5f) / numPoints) * radius;
             float theta = 2 * Mathf.PI * i * turnFraction; // (1 + Mathf.Sqrt(5));
 
             float x = dst * Mathf.Cos(theta);
@@ -56,12 +86,29 @@ public class GeneratePoints : MonoBehaviour
             points[i].transform.position = new Vector3(x, y, 0);
             }
 
-            for (int i = n; i < 300; i++) {
+            for (int i = (int) numPoints; i < 300; i++) {
                 points[i].SetActive(false);
             }
 
-            n_cache = n;
-            turn_cache = turnFraction;
+            n = (int) numPoints;
+        }
+    }
+
+    public void RefreshTurnFrac(float turnFrac)
+    {
+        if (0 <= turnFrac && turnFrac <= 3) {
+
+            for (int i = 0; i < n; i++) {
+            float dst = Mathf.Sqrt((i + 0.5f) / n) * radius;
+            float theta = 2 * Mathf.PI * i * turnFrac; // (1 + Mathf.Sqrt(5));
+
+            float x = dst * Mathf.Cos(theta);
+            float y = dst * Mathf.Sin(theta);
+
+            points[i].transform.position = new Vector3(x, y, 0);
+            }
+
+            turnFraction = turnFrac;
         }
     }
 }
